a80a2fe2e10f492d1b39a6242f66c73a1b4c1070
Antoine TACAIL--CASES <74400261+Stay-Determined@users.noreply.github.com>
2023-03-01 18:28:44 +0100

    ajout des tests get post et put

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from mongoengine import connect
@@ -10,18 +10,6 @@ app = FastAPI()
 
 connect(db="test", host="localhost", port=27017)
 
-
-@app.get("/")
-def home():
-    return {"message":"Hello World!"} 
-
-#Ancienne version avec mongoengine
-#@app.get("/get_all_tests")
-#def get_all_tests():
-#    tests_list = json.loads(Test.objects().to_json())
-#    return{"tests": tests_list}
-
-
 @app.get("/get_test")
 def get_test():
     
@@ -31,7 +19,7 @@ def get_test():
     #print(list(collection.find({},{"_id":0})))
     x = jsonable_encoder(list(collection.find({},{"_id":0})))
     return JSONResponse(content=x)
-
+    return{"Récupération effectuée"}
 
 
 @app.delete("/delete_test")
@@ -41,6 +29,7 @@ def delete_test(age: str):
     collection = dataBase["test"]
     myQuery = {'age':age}
     collection.delete_one(myQuery)
+    return{"Suppression effectuée"}
 
 @app.post("/add_test")
 def add_test(nom: str, age: str, loisir: str):
@@ -49,6 +38,7 @@ def add_test(nom: str, age: str, loisir: str):
     collection = dataBase["test"]
     myQuery = {'nom':nom, 'age':age, 'loisir': loisir}
     collection.insert_one(myQuery)
+    return{"Test ajouté"}
     
 @app.put("/update_test")
 def update_test(oldname: str, nom: str,age: str,loisir: str):
@@ -58,4 +48,4 @@ def update_test(oldname: str, nom: str,age: str,loisir: str):
     myOldQuery = {"nom":oldname}
     myQuery = {"$set":{"nom":nom, "age": age, "loisir": loisir}}
     collection.update_one(myOldQuery,myQuery)
-    return{"test_supp":get_test()}
+    return{"MAJ effectuée"}
--- test_main.py
+++ test_main.py
@@ -1,26 +1,50 @@
-from pymongo import MongoClient
 import unittest
-import model
-import main
-import requests
+from pymongo import MongoClient
+from fastapi.testclient import TestClient
+from main import app
+import json
 
-class mainTest(unittest.TestCase):
-    def test_get_test(self):
-        reponse = requests.get("http://localhost:8000/test")
-        self.assertEqual(reponse.status_code, 200)
-        self.assertIsInstance(reponse.json(),list)
+class TestCrudRequest(unittest.TestCase):
+    
+    def setUp(self):
+        #Connexion à la base de donnée mongodb nommé "test" avec une collection nommé "test"
+        #C'est dans le setUp pour ne pas à avoir à le refaire dans chaque test, cela devient illisible
+        
+        self.client = MongoClient('localhost', 27017)
+        self.db = self.client['test']
+        self.collection = self.db['test']
+        self.test_client = TestClient(app)
+        
+        
+        
+    def test_post(self):
+        retour = self.test_client.post("/add_test", params={"nom": "a", "age": "0", "loisir": "aaa"} )
+        self.assertEqual(retour.status_code, 200)
+        self.assertEqual(retour.json(), ["Test ajouté"]) 
+        ajout = self.collection.find_one({"nom": "a"})
+        self.assertIsNotNone(ajout)
+        self.assertEqual(ajout["age"], "0")
+        self.assertEqual(ajout["loisir"], "aaa")
+        
+    def test_get(self):
+        # Test de la requête GET (en supposant que les données nom/age/loisir sont a/0/aaa )
+        retour = self.test_client.get("/get_test")
+        self.assertEqual(retour.status_code, 200)
+        test_utilisateur = json.loads(retour.content)
+        self.assertEqual(test_utilisateur[0]["nom"], "a")
+        self.assertEqual(test_utilisateur[1]["age"], "0")
+        self.assertEqual(test_utilisateur[2]["loisir"], "aaa")
+        self.assertEqual(retour.json(), ["Récupération effectuée"]) 
 
-    def test_delete_test(self):
-        reponse = requests.get("http://localhost:8000/test")
-        self.assertEqual(reponse.status_code, 200)
+    def test_put(self):
+
+        retour = self.test_client.put('/update_test', params = {"oldname": "a", "nom": "b", "age": "2", "loisir": "bbb"})
         
-    def test_add_test(self):
-        reponse = requests.get("http://localhost:8000/test")
-        self.assertEqual(reponse.status_code, 200)
-        
-    
-    def test_update_test(self):
-        reponse = requests.get("http://localhost:8000/test")
-        self.assertEqual(reponse.status_code, 200)
-        
-    
+        self.assertEqual(retour.status_code, 200)
+        self.assertEqual(retour.json(), ["MAJ effectuée"]) 
+        updated_doc = self.collection.find_one({"nom": "b"})
+        self.assertEqual(updated_doc["age"], "2")
+        self.assertEqual(updated_doc["loisir"], "bbb")
+
+if __name__ == '__main__':
+    unittest.main()
